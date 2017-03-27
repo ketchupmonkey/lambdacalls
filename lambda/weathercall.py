bd6d096f2896da17d69023861b277de50ce1a510
ketchupmonkey <shimmallonga@gmail.com>
2017-03-28 00:31:58 +1300

    Update weathercall.py

--- lambda/weathercall.py
+++ lambda/weathercall.py
@@ -6,7 +6,7 @@ import simplejson
 api_url = "http://api.openweathermap.org"
 def weathercall():
 
-  url = "{}{}" . format(api_url, '/data/2.5/weather?id=2193733&APPID=5af0611c68527acab60a69445dbb28e1&units=metric')
+  url = "{}{}" . format(api_url, '/data/2.5/weather?id=2193733&APPID=&units=metric')
   result=requests.get(url)
   # print result
   print url
